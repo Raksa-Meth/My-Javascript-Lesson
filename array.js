7af2cba28a640286e8e4765b852f6297ad8d8bcd
Raksa <raksa@Raksas-MacBook-Pro.local>
2020-12-06 17:55:11 +0700

    add Array

---
+++ array.js
@@ -0,0 +1,10 @@
+//create array and init the value
+let mArray = ['one','two','three','four']
+
+//add more element to the array
+mArray.push('five')
+//remove element
+mArray.pop()
+//access array
+console.log(mArray[0])
+console.log(mArray[1])
